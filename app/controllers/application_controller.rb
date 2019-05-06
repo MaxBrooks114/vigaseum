965d8c1f35db744dba02bdfc976663b1ce041b78
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-05-06 17:14:39 -0400

    added helper methods added form action and method to login form created signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,22 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+
+  helpers do
+   def redirect_if_not_logged_in
+     if !logged_in?
+       redirect "/login?error=You have to be logged in to do that"
+     end
+   end
+
+   def logged_in?
+     !!session[:user_id]
+   end
+
+   def current_user
+     User.find(session[:user_id])
+   end
+
+ end
+
 end
