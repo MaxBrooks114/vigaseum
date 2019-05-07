4671371e66afc87ef021e6996776b7a8602b6e34
Max Brooks <Maxbrooks114@gmail.com>
2019-05-07 15:00:54 +0000

    Merge branch 'master' of https://github.com/MaxBrooks114/vigaseum

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190506213737) do
+ActiveRecord::Schema.define(version: 20190507143847) do
+
+  create_table "consoles", force: :cascade do |t|
+    t.string  "name"
+    t.string  "company"
+    t.string  "games"
+    t.date    "date_added"
+    t.integer "generation"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
