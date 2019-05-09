aa497fc504acade2d8c44eaba4f41a37ed5409c1
Max Brooks <Maxbrooks114@gmail.com>
2019-05-09 17:25:24 +0000

    added set method override to fix patch route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
     enable :sessions
     set :session_secret, "vigaseum"
   end
