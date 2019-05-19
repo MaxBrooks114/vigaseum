c0f3298554ab8e349d81bca3e3b5ea9c997e6b6d
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-05-19 12:28:02 -0400

    got rid of rack flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 require './config/environment'
-require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,7 +8,6 @@ class ApplicationController < Sinatra::Base
     set :method_override, true
     enable :sessions
     set :session_secret, "vigaseum"
-    use Rack::Flash
   end
 
   get "/" do
